4f4e6357837053e52a0964f798fc9651a02112f7
Prakash Badhe <prakash.badhe@vishwasoft.in>
2021-03-16 00:54:36 +0530

    scsa

--- vars/buildMavenStep.groovy
+++ vars/buildMavenStep.groovy
@@ -1,7 +1,7 @@
 //vars/buildMavenStep
 def call(Map config) {
     node {
-        git url: "https://github.com/${config.user}/${config.repo}.git"
+        git branch: 'main', credentialsId: 'pr-jenkins-user',  url: "https://github.com/${config.user}/${config.repo}.git"
         sh 'mvn install'
         //mail to: '...', subject: "${config.name} app is build", body: '...'
         echo "The maven app ${config.repo} is built"
