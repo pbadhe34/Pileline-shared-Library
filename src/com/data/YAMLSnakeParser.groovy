3448ba9f355c4873ea0a9442d9ee7df3431b6b89
Prakash Badhe <prakash.badhe@vishwasoft.in>
2021-03-16 02:37:57 +0530

    sss

---
+++ src/com/data/YAMLSnakeParser.groovy
@@ -0,0 +1,19 @@
+package com.data
+
+ @Grab('org.yaml:snakeyaml:1.17')
+
+import org.yaml.snakeyaml.Yaml
+
+class YAMLSnakeParser{
+
+ String parseYmlData(filename){ 
+Yaml parser = new Yaml()
+data = parser.load((filename as File).text)
+
+  data.each{println it}
+  println data.records.car.name
+ 
+ println data.records.car.record.model.dealer.name
+  data
+  }
+ }
