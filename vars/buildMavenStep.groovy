17544a39ffd8c4f88e78c6660c23282ef6886605
Prakash Badhe <prakash.badhe@vishwasoft.in>
2021-03-16 01:29:19 +0530

    sdsd

--- vars/buildMavenStep.groovy
+++ vars/buildMavenStep.groovy
@@ -3,7 +3,7 @@ def call(Map config) {
     node {
         git branch: 'main', credentialsId: 'pr-jenkins-user',  url: "https://github.com/${config.user}/${config.repo}.git"
         
-        sh "export PATH='/home/dell/Desktop/Jenkins-CI-Introduction/Day4/apache-maven-3.5.0/bin:$PATH'"
+        sh 'export PATH=/home/dell/Desktop/Jenkins-CI-Introduction/Day4/apache-maven-3.5.0/bin:$PATH'
    
          sh 'mvn package'
 
