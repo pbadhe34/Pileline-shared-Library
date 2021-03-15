fd1724490f2b191522988f85e9a420309cda6efa
Prakash Badhe <prakash.badhe@vishwasoft.in>
2021-03-16 03:18:40 +0530

    ss

--- src/com/data/YAMLSnakeParser.groovy
+++ src/com/data/YAMLSnakeParser.groovy
@@ -7,17 +7,12 @@ import org.yaml.snakeyaml.Yaml
 class YAMLSnakeParser{
 
  String parseStructure(filename){ 
-  println "starting with $filename"
+  
  Yaml parser = new Yaml()
  
- def data = parser.load((filename as File).text)
-
-  println "parse done"
-
-  //data.each{println it}
-  println data.records.car.name
+ def data = parser.load((filename as File).text)  
  
- println data.records.car.record.model.dealer.name
+  
    data
   }
  }
