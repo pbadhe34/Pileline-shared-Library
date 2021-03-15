d31592f988e6e1dc24abe9f58a199901682e2a02
Prakash Badhe <prakash.badhe@vishwasoft.in>
2021-03-16 01:41:36 +0530

    ascas

---
+++ src/com/data/JSONFileReader.groovy
@@ -0,0 +1,35 @@
+ 
+package com.data
+
+import groovy.json.JsonSlurper
+import java.io.File
+import java.io.Reader
+import java.io.FileReader
+import java.io.BufferedReader
+
+ class JSONFileReader{
+
+  String readJSONTreeData(filename){
+
+ def slurper = new JsonSlurper() 
+  
+
+ BufferedReader inReader = new BufferedReader(new FileReader(filename));
+
+ Reader readJson = inReader ;
+  
+ def result = slurper.parse(readJson)
+
+
+
+ //println "The json result is  "+result
+ 
+ 
+ //println "User name is $result.users.user3.name"
+ //println "User age is $result.users.user3.age"
+ //println "User balance is $result.users.user3.balance"
+  result
+   }
+  }
+  
+
