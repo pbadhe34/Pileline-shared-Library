e504320374c9e764d881f5aaf7e801f72ca1185a
Prakash Badhe <prakash.badhe@vishwasoft.in>
2021-03-17 00:35:48 +0530

    BuilMoavenStep

--- vars/buildMavenStep.groovy
+++ vars/buildMavenStep.groovy
@@ -2,10 +2,10 @@
 def call(Map config) {
     node {
         git branch: 'main', credentialsId: 'pr-jenkins-user',  url: "https://github.com/${config.user}/${config.repo}.git"
-        
-        sh 'export PATH=/home/dell/Desktop/Jenkins-CI-Introduction/Day4/apache-maven-3.5.0/bin:$PATH'
-   
-         sh 'mvn package'
+        def mvnHome = tool 'maven3.5'
+        /*sh 'export PATH=/home/dell/Desktop/Jenkins-CI-Introduction/Day4/apache-maven-3.5.0/bin:$PATH'   
+*/     
+         sh  "${mvnHome}/bin/mvn package'        
 
         //mail to: '...', subject: "${config.name} app is build", body: '...'
         echo "The maven app ${config.repo} is built"
